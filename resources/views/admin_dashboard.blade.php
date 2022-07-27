0199ede94c12722b036d62b53f49bc9c5614493f
opusaha <sahaapo@gmail.com>
2022-07-27 15:49:55 +0600

    web template with login system

---
+++ resources/views/admin_dashboard.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-green-800 leading-tight">
+            {{ __('Admin_dashboard') }}
+        </h2>
+    </x-slot>
+
+</x-app-layout>
