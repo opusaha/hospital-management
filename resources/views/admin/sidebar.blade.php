38c470cef638b868582fe100c06391596fe56422
opusaha <sahaapo@gmail.com>
2022-08-02 10:29:46 +0600

    admin nav and side customized

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,6 +1,6 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
         <div class="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
-          <a class="sidebar-brand brand-logo" href="index.html"><img src="admin/assets/images/logo.svg" alt="logo" /></a>
+          <p class="sidebar-brand brand-logo text-light">Admin Panel</p>
           <a class="sidebar-brand brand-logo-mini" href="index.html"><img src="admin/assets/images/logo-mini.svg" alt="logo" /></a>
         </div>
         <ul class="nav">
@@ -13,7 +13,6 @@
                 </div>
                 <div class="profile-name">
                   <h5 class="mb-0 font-weight-normal">Admin</h5>
-                  <span>Gold Member</span>
                 </div>
               </div>
               <a href="#" id="profile-dropdown" data-bs-toggle="dropdown"><i class="mdi mdi-dots-vertical"></i></a>
@@ -73,6 +72,14 @@
             </a>
           </li>
           <li class="nav-item menu-items">
+            <a class="nav-link" href="{{url('appointment')}}">
+              <span class="menu-icon">
+                <i class="mdi mdi-gamepad-round"></i>
+              </span>
+              <span class="menu-title">Appointment</span>
+            </a>
+          </li>
+          <li class="nav-item menu-items">
             <a class="nav-link" href="{{url('add-patient')}}">
               <span class="menu-icon">
                 <i class="mdi mdi-baby"></i>
@@ -86,14 +93,6 @@
                 <i class="mdi mdi-human-female"></i>
               </span>
               <span class="menu-title">Add Nurse</span>
-            </a>
-          </li>
-          <li class="nav-item menu-items">
-            <a class="nav-link" href="{{url('appointment')}}">
-              <span class="menu-icon">
-                <i class="mdi mdi-gamepad-round"></i>
-              </span>
-              <span class="menu-title">Appointment</span>
             </a>
           </li>
         </ul>
